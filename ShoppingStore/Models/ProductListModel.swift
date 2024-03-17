678a949d42702ef21e0107e70b2727637c821d65
usitha5555 <usithaarachchiofficial@gmail.com>
2024-03-17 09:54:27 +0530

    api

--- ShoppingStore/Models/ProductListModel.swift
+++ ShoppingStore/Models/ProductListModel.swift
@@ -1,8 +1,32 @@
 //
-//  ProductListModel.swift
+//  ProductResponse.swift
 //  ShoppingStore
 //
-//  Created by Usitha Kodithuwakku Arachchi on 2024-03-17.
+//  Created by Usitha Kodithuwakku Arachchi on 2024-03-16.
 //
 
 import Foundation
+
+// MARK: - Welcome
+struct ProductResponse: Codable {
+    let data: [Items]
+    let success: Bool
+    let message: String
+}
+
+// MARK: - Datum
+struct Items: Codable {
+    let id: Int
+    let name, description, price, category: String
+    let size, color: String
+    let image: String
+    let quantity: Int
+    let createdAt, updatedAt: String
+
+    enum CodingKeys: String, CodingKey {
+        case id, name, description, price, category, size, color, image, quantity
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
+
--- ShoppingStore/ViewModels/ProductViewModel.swift
+++ ShoppingStore/ViewModels/ProductViewModel.swift
@@ -1,8 +1,41 @@
-//
-//  ProductViewModel.swift
-//  ShoppingStore
-//
-//  Created by Usitha Kodithuwakku Arachchi on 2024-03-17.
-//
 
 import Foundation
+
+import SwiftUI
+
+
+class ProductViewModel : ObservableObject {
+    
+    @Published var productResults : [Items] = []
+    
+    let baseUrl = "http://127.0.0.1:8000/items";
+    
+    init() {
+        loadProducts()
+    }
+    
+    func loadProducts() {
+        guard let url = URL(string: baseUrl) else {return}
+        
+        URLSession(configuration: .default).dataTask(with:
+                                                        URLRequest(url: url)) {data, response, error in
+            
+            guard let data = data else {return}
+            
+            do {
+                let productResponse = try JSONDecoder().decode(ProductResponse.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.productResults = productResponse.data
+                }
+                let productResults = productResponse.data
+                print(productResults)
+            }
+            catch{
+                print("Unable to decode the data!!")
+            }
+        }.resume()
+    }
+}
+
+
